3afc6c44d67f9b46744c35db4d353c38666fecf6
batch11devops <rakshashetty722@gmail.com>
2025-08-20 15:38:50 +0530

    Update and rename variable.tf to variables.tf

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "subscription_id" {}
+variable "client_id" {}
+variable "client_secret" {}
+variable "tenant_id" {}
+variable "resource_group_name" {
+  default = "rg-vmss-budget"
+}
+variable "location" {
+  default = "Japan East"
+}
+
