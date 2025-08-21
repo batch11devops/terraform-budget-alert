5ca59b85b11c1401cd0110465a2b5ac03d866798
batch11devops <rakshashetty722@gmail.com>
2025-08-21 11:18:48 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -2,17 +2,21 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~>4.0"
+      version = ">=4.0"
     }
+  }
+  backend "azurerm" {
+    resource_group_name  = "tfstate-rg"
+    storage_account_name = "tfstatestorage"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
   }
 }
 
 provider "azurerm" {
-  features {}
-
+  features        = {}
   subscription_id = var.subscription_id
   client_id       = var.client_id
   client_secret   = var.client_secret
   tenant_id       = var.tenant_id
 }
-
