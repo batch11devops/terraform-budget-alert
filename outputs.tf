ff398e680a7da6a85b452a444f3bfacd8a01d1b5
batch11devops <rakshashetty722@gmail.com>
2025-08-21 11:22:14 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,7 @@
-output "resource_group_name" {
-  value = azurerm_resource_group.rg.name
-}
-
 output "vmss_name" {
   value = azurerm_linux_virtual_machine_scale_set.vmss.name
 }
 
 output "budget_name" {
-  value = azurerm_consumption_budget_subscription.budget.name
+  value = azurerm_consumption_budget_resource_group.budget.name
 }
