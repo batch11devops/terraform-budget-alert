5d6aa22bdcd49bbaf6859b74b47842ff5849355d
batch11devops <rakshashetty722@gmail.com>
2025-08-22 10:37:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,18 +1,41 @@
+# ------------------------------
 # Resource Group
+# ------------------------------
 resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
   location = var.location
 }
 
-# VM Scale Set
+# ------------------------------
+# Networking (simple VNet+Subnet)
+# ------------------------------
+resource "azurerm_virtual_network" "vnet" {
+  name                = "${var.resource_group_name}-vnet"
+  address_space       = [var.vnet_cidr]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "default"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = [var.subnet_cidr]
+}
+
+# ------------------------------
+# VM Scale Set (minimal)
+# ------------------------------
 resource "azurerm_linux_virtual_machine_scale_set" "vmss" {
   name                = var.vmss_name
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
-  sku                 = "Standard_DS1_v2"
-  instances           = var.vmss_instances
-  admin_username      = var.admin_username
-  admin_password      = var.admin_password
+
+  sku       = var.vmss_sku
+  instances = var.vmss_instances
+
+  admin_username = var.admin_username
+  admin_password = var.admin_password
 
   source_image_reference {
     publisher = "Canonical"
@@ -25,14 +48,29 @@ resource "azurerm_linux_virtual_machine_scale_set" "vmss" {
     storage_account_type = "Standard_LRS"
     caching              = "ReadWrite"
   }
+
+  network_interface {
+    name    = "${var.vmss_name}-nic"
+    primary = true
+
+    ip_configuration {
+      name                                   = "internal"
+      primary                                = true
+      subnet_id                              = azurerm_subnet.subnet.id
+      load_balancer_backend_address_pool_ids = [] # none for sample
+    }
+  }
 }
 
-# Budget Alert
+# ------------------------------
+# Budget (Resource Group scope)
+# ------------------------------
 resource "azurerm_consumption_budget_resource_group" "budget" {
-  name              = "vmss-budget-alert"
+  name              = var.budget_name
   resource_group_id = azurerm_resource_group.rg.id
-  amount            = var.budget_amount
-  time_grain        = "Monthly"
+
+  amount     = var.budget_amount
+  time_grain = "Monthly"
 
   time_period {
     start_date = var.budget_start_date
@@ -41,8 +79,9 @@ resource "azurerm_consumption_budget_resource_group" "budget" {
 
   notification {
     enabled        = true
-    threshold      = var.budget_threshold
     operator       = "GreaterThan"
+    threshold      = var.budget_threshold
     contact_emails = var.budget_emails
+    threshold_type = "Actual"
   }
 }
