7b788d41310a4293eebad3a4791fc9b4a15dc017
batch11devops <rakshashetty722@gmail.com>
2025-08-22 10:25:47 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,22 +1,24 @@
 terraform {
+  required_version = ">= 1.5.0"
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">=4.0"
+      version = ">= 3.110.0"
     }
   }
-  backend "azurerm" {
-    resource_group_name  = "tfstate-rg"
-    storage_account_name = "tfstatestorage"
-    container_name       = "tfstate"
-    key                  = "terraform.tfstate"
-  }
+
+  # Backend values are passed by Jenkins during `terraform init -backend-config=...`
+  backend "azurerm" {}
 }
 
 provider "azurerm" {
-  features        = {}
+  features {}
+
+  # If you export ARM_* in Jenkins env, you can omit these vars entirely.
   subscription_id = var.subscription_id
   client_id       = var.client_id
   client_secret   = var.client_secret
   tenant_id       = var.tenant_id
 }
+
